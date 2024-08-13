5d67f39819d7a0ce2ec55548463644ce74d8aec7
qsliu <qsliu2017@outlook.com>
2024-08-13 19:27:55 +0800

    modified

--- postgres-scanner-tpch.py
+++ postgres-scanner-tpch.py
@@ -1,28 +1,24 @@
 import duckdb
 import psycopg2
-import sqlite3
-import os
 import tempfile
 import time
-import subprocess
 
+DUCKDB_DATABASE_FILE = "tpch.duckdb"
+POSTGRES_CONNECTION_STRING = "dbname=tpch"
+# set a one-minute timeout for postgres to avoid waiting forever
+POSTGRES_TIMEOUT_MS = 30000
+POSTGRES_WORKERS = 10
 
 # connect to databases
-duckdb_database_file = 'tpch.duckdb'
-dcon = duckdb.connect(duckdb_database_file)
-
-postgres_connection_string = 'dbname=tpch'
-pcon = psycopg2.connect(postgres_connection_string).cursor()
-pcon.execute('ROLLBACK') # ?!
-
-# sqlite_database_file = 'tpch.sqlite'
-# scon = sqlite3.connect(sqlite_database_file).cursor()
+dcon = duckdb.connect(DUCKDB_DATABASE_FILE)
+pcon = psycopg2.connect(POSTGRES_CONNECTION_STRING).cursor()
+pcon.execute("ROLLBACK")  # ?!
 
 # install and load DuckDB extensions required
-dcon.execute('INSTALL tpch')
-dcon.execute('LOAD tpch')
+dcon.execute("INSTALL tpch")
+dcon.execute("LOAD tpch")
 
-schemascript = '''
+schemascript = """
 
 CREATE TABLE NATION  ( N_NATIONKEY  INTEGER NOT NULL,
                             N_NAME       CHAR(25) NOT NULL,
@@ -93,53 +89,34 @@ CREATE TABLE LINEITEM ( L_ORDERKEY    INTEGER NOT NULL,
                              L_SHIPMODE     CHAR(10) NOT NULL,
                              L_COMMENT      VARCHAR(44) NOT NULL, PRIMARY KEY (L_ORDERKEY, L_LINENUMBER));
 
-                             '''
+                             """
+
 
 def lineitem_exists(con):
-	try:
-		con.execute("SELECT * FROM lineitem")
-		return True
-	except:
-		return False
+    try:
+        con.execute("SELECT * FROM lineitem")
+        return True
+    except:
+        return False
+
 
 # create the tpch test data in duckdb if required
 if not lineitem_exists(dcon):
-	dcon.execute('CALL dbgen(sf=1)')
+    dcon.execute("CALL dbgen(sf=1)")
 
-exportdir = ''
-if not lineitem_exists(pcon) or not lineitem_exists(scon):
-	exportdir = str(tempfile.mkdtemp())
-	dcon.execute("EXPORT DATABASE '%s' (FORMAT CSV)" % exportdir)
-
-# if not lineitem_exists(scon):
-
-# 	loadscript = schemascript + '''
-# .headers off
-# .sep ,
-# .import DIR/nation.csv nation
-# .import DIR/region.csv region
-# .import DIR/part.csv part
-# .import DIR/supplier.csv supplier
-# .import DIR/partsupp.csv partsupp
-# .import DIR/customer.csv customer
-# .import DIR/orders.csv orders
-# .import DIR/lineitem.csv lineitem
-# 	'''.replace('DIR', exportdir)
-
-# 	# somewhat ghetto but well
-# 	p = subprocess.run(['sqlite3', '-batch', sqlite_database_file], input=loadscript, capture_output=True, text=True)
-# 	if p.returncode != 0:
-# 		raise ValueError(p.stderr)
-
-
+exportdir = ""
 # create the tpch test data in postgres if required
 if not lineitem_exists(pcon):
-	# FIXME do the actual load?
-	pcon.execute(open('%s/schema.sql' % exportdir, 'rb').read())
-	pcon.execute(open('%s/load.sql' % exportdir, 'rb').read())
+    exportdir = str(tempfile.mkdtemp())
+    dcon.execute("EXPORT DATABASE '%s' (FORMAT CSV)" % exportdir)
 
-	# some foreign keys for postgres to be fair
-	pcon.execute('''
+    # FIXME do the actual load?
+    pcon.execute(open("%s/schema.sql" % exportdir, "rb").read())
+    pcon.execute(open("%s/load.sql" % exportdir, "rb").read())
+
+    # some foreign keys for postgres to be fair
+    pcon.execute(
+        """
 
 			ALTER TABLE PART
 	  ADD CONSTRAINT part_kpey
@@ -206,71 +183,59 @@ if not lineitem_exists(pcon):
 	ADD CONSTRAINT nation_region_fkey
 	   FOREIGN KEY (N_REGIONKEY) REFERENCES REGION(R_REGIONKEY);
 
-		 ''')
+		 """
+    )
 
-	pcon.execute('COMMIT; CHECKPOINT; ANALYZE;')
+    pcon.execute("COMMIT; CHECKPOINT; ANALYZE;")
 
-# set a one-minute timeout for postgres to avoid waiting forever
-postgres_timeout_ms = 30000
-postgres_workers = 10
+pcon.execute("SET statement_timeout = %d" % POSTGRES_TIMEOUT_MS)
+pcon.execute("SET max_parallel_workers_per_gather = %d" % POSTGRES_WORKERS)
+# pcon.execute('SET max_worker_processes = %d' % postgres_workers)
+pcon.execute("SET max_parallel_workers = %d" % POSTGRES_WORKERS)
 
-pcon.execute('SET statement_timeout = %d' % postgres_timeout_ms)
-pcon.execute('SET max_parallel_workers_per_gather = %d' % postgres_workers)
-#pcon.execute('SET max_worker_processes = %d' % postgres_workers)
-pcon.execute('SET max_parallel_workers = %d' % postgres_workers)
-
+# duckdb + postgres scanner
 dcon3 = duckdb.connect()
-dcon3.execute('INSTALL postgres_scanner')
-dcon3.execute('LOAD postgres_scanner')
-dcon3.execute("CALL postgres_attach('%s', filter_pushdown=true)" % postgres_connection_string)
+dcon3.execute("INSTALL postgres_scanner")
+dcon3.execute("LOAD postgres_scanner")
+dcon3.execute(
+    "CALL postgres_attach('%s', filter_pushdown=true)" % POSTGRES_CONNECTION_STRING
+)
 
-# dcon4 = duckdb.connect(config={'allow_unsigned_extensions' : True})
-# dcon3.execute('INSTALL sqlite_scanner')
-# dcon4.execute('LOAD sqlite_scanner')
-# dcon4.execute("CALL sqlite_attach('%s')" % sqlite_database_file)
+# postgres + pg_duckdb
+pcon4 = psycopg2.connect(POSTGRES_CONNECTION_STRING).cursor()
+pcon4.execute("ROLLBACK")  # ?!
+pcon4.execute("SET duckdb.execution = true")
+pcon4.execute("SET statement_timeout = %d" % POSTGRES_TIMEOUT_MS)
+pcon4.execute("SET max_parallel_workers_per_gather = %d" % POSTGRES_WORKERS)
+# pcon4.execute('SET max_worker_processes = %d' % postgres_workers)
+pcon4.execute("SET max_parallel_workers = %d" % POSTGRES_WORKERS)
 
 
 def timeq(con, q):
-	start = time.time()
-	try:
-		con.execute(q)
-	except psycopg2.errors.QueryCanceled:
-		con.execute('ROLLBACK')
-		pcon.execute('SET statement_timeout = %d' % postgres_timeout_ms)
-		return 'timeout'
-	except sqlite3.OperationalError as e:
-		return str(e)
-	# except:
-	# 	return 'failed'
-	end = time.time()
-	return round(end - start, 2)
+    start = time.time()
+    try:
+        con.execute(q)
+    except psycopg2.errors.QueryCanceled:
+        con.execute("ROLLBACK")
+        pcon.execute("SET statement_timeout = %d" % POSTGRES_TIMEOUT_MS)
+        return "timeout"
+    end = time.time()
+    return round(end - start, 2)
 
 
 print("experiment\tquery\ttime")
 
-queries = dcon.execute('SELECT * FROM tpch_queries()').fetchall()
+queries = dcon.execute("SELECT * FROM tpch_queries()").fetchall()
 
 for i in range(5):
-	for q in queries:
-		print("duckdb\t%d\t%s" % (q[0], timeq(dcon, q[1])))
+    for q in queries:
+        print("duckdb\t%d\t%s" % (q[0], timeq(dcon, q[1])))
 
-	for q in queries:
-		print("postgres\t%d\t%s" % (q[0], timeq(pcon, q[1])))
+    for q in queries:
+        print("postgres\t%d\t%s" % (q[0], timeq(pcon, q[1])))
 
-	for q in queries:
-		print("duckdb/postgres\t%d\t%s" % (q[0], timeq(dcon3, q[1])))
+    for q in queries:
+        print("duckdb + postgres_scanner\t%d\t%s" % (q[0], timeq(dcon3, q[1])))
 
-	# for q in queries:
-	# 	print("duckdb/sqlite3\t%d\t%s" % (q[0], timeq(dcon4, q[1])))
-
-	# for q in queries:
-	# 	if int(q[0]) == 17 or int(q[0]) == 20 or int(q[0]) == 22: # no idea how to set a timeout for sqlite in python
-	# 		print("sqlite\t%d\t%s" % (q[0], 'timeout'))
-	# 		continue
-	# 	# using queries from here instead: https://github.com/ibis-project/tpc-queries/tree/master/sqlite_tpc
-	# 	# cause sqlite does not like the postgres/duckdb syntax very much
-	# 	altq = open('../tpc-queries/sqlite_tpc/h%02d.sql' % int(q[0]), 'rb').read().decode('utf8')
-	# 	print("sqlite\t%d\t%s" % (q[0], timeq(scon, altq)))
-
-
-
+    for q in queries:
+        print("postgres + pg_duckdb\t%d\t%s" % (q[0], timeq(pcon4, q[1])))
